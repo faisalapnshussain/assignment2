bea4b5ff00376a2656dab0f8fda57404fce5405d
Faisal Hussain <faisalapnshussain@gmail.com>
2024-10-28 00:04:44 +0500

    first commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,8 @@ const Contact = () => {
         <p className='text-white mt-2 mx-2'>Office in Karachi</p>
         <p className='text-white mt-2 mx-4 font-extralight font-sans'>21/3, Sector No. 22, Korangi Industrial Area, Karachi-74900 G.P.O.Box 940
         <br/>Ph:021-3504248
-        </p><div className='float-left'>        <img className="h-100 justify-center pl-20"  src="/images/map.png" alt="image description" /></div>
+        </p><div className='float-left'>      
+            <img className="h-100 justify-center pl-20"  src="/images/map.png" alt="image description" /></div>
 
       </div>
   )
